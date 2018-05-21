5d219c439840275058bca4cf88e9c435050cf04e
ünzile <unzile6167@gmail.com>
2018-05-21 14:32:14 +0300

    Add files via upload

---
+++ arkadasOneri/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace arkadasOneri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.muhendislikDataSet = new arkadasOneri.muhendislikDataSet();
+            this.aktiviteBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.aktiviteTableAdapter = new arkadasOneri.muhendislikDataSetTableAdapters.aktiviteTableAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.muhendislikDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aktiviteBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.Form1_Load);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SlateGray;
+            this.button1.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(257, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Arkadaş Öner";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // muhendislikDataSet
+            // 
+            this.muhendislikDataSet.DataSetName = "muhendislikDataSet";
+            this.muhendislikDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // aktiviteBindingSource
+            // 
+            this.aktiviteBindingSource.DataMember = "aktivite";
+            this.aktiviteBindingSource.DataSource = this.muhendislikDataSet;
+            // 
+            // aktiviteTableAdapter
+            // 
+            this.aktiviteTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.CadetBlue;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(5, 143);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(510, 158);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(517, 304);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Arkadaş Öneri Sistemi";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.muhendislikDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aktiviteBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private muhendislikDataSet muhendislikDataSet;
+        private System.Windows.Forms.BindingSource aktiviteBindingSource;
+        private muhendislikDataSetTableAdapters.aktiviteTableAdapter aktiviteTableAdapter;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ arkadasOneri/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace arkadasOneri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //veritabanı baglantı cumlecigi veritabanı adı=muhendislik 
+        SqlConnection baglanti = new SqlConnection(@"server=UNZILE\SQLEXPRESS; Initial Catalog=muhendislik; Integrated Security=SSPI");
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'muhendislikDataSet.aktivite' table. You can move, or remove it, as needed.
+            this.aktiviteTableAdapter.Fill(this.muhendislikDataSet.aktivite);
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open(); //veritabanı baglantısını actıkm
+            List<string> arkadasOlanlar = new List<string>(); //girilen numaraya göre olusturulan arkadasların listesinin tutulacağı list
+            string arkadaslar = "Select * From arkadas Where  (OgrNo=" + textBox1.Text + ")"; //girilen numaranın arkadaslarını cekecek sql sorgusu
+            SqlCommand komut = new SqlCommand(arkadaslar, baglanti); //sql sorgusunun veritabanı ile bağlantısını sağladm
+            SqlDataReader dr = komut.ExecuteReader();
+
+            if (!dr.HasRows)
+            {
+                MessageBox.Show("Bu numara sistemde bulunmamaktadır");
+            }
+          
+            while (dr.Read())
+                {
+                    //DataReader'daki verileri arkadasolanlar adındaki listte attım
+                    arkadasOlanlar.Add(dr["ark1"].ToString());
+                    arkadasOlanlar.Add(dr["ark2"].ToString());
+                    arkadasOlanlar.Add(dr["ark3"].ToString());
+                    arkadasOlanlar.Add(dr["ark4"].ToString());
+                    arkadasOlanlar.Add(dr["ark5"].ToString());
+                    arkadasOlanlar.Add(dr["ark6"].ToString());
+                    arkadasOlanlar.Add(dr["ark7"].ToString());
+                    arkadasOlanlar.Add(dr["ark8"].ToString());
+                    arkadasOlanlar.Add(dr["ark9"].ToString());
+                    arkadasOlanlar.Add(dr["ark10"].ToString());
+
+
+                }
+
+
+            dr.Close();
+                DataTable dt = new DataTable(); //verilerimi atacagım bir dataTable olusturdum
+
+                foreach (string parca in arkadasOlanlar) // arkadaslarOlanlar listini gezinerek arkadaşların aktivitelerini getirilmesi
+                {
+
+                    string ara = "Select * From aktivite Where (OgrNo=" + parca + ")"; //arkadasların öğrenci numaralarına göre aktivite bilgilerini cektim
+                    SqlCommand komut2 = new SqlCommand(ara, baglanti); //veritabanı ile sql sorgusu arasında baglantıyı kurdum
+                    SqlDataAdapter da = new SqlDataAdapter(komut2);
+                    da.Fill(dt); //dataTable içerisini sql sorgusu sonucu gelen verilerle doldurdum
+
+                }
+
+           
+
+            dataGridView1.DataSource = dt; //datatable icerigin gride atarak ekranda görülmesini sağladım.
+            
+            //string arkadasOlmayan = "SELECT * FROM arkadas WHERE OgrNo not in (" + textBox1.Text + arkadasOlanlar["ark1"]+")";
+            //SqlCommand kmt = new SqlCommand(arkadasOlmayan, baglanti);
+            //SqlDataAdapter daa = new SqlDataAdapter(kmt);
+            //MessageBox.Show(arkadasOlmayan);
+            //daa.Fill(dt);
+            //dataGridView2.DataSource = dt;
+
+            //List<string> arkadasOlmayanlarınIlkkismi = new List<string>();
+            //int i = 0;
+            //while(i != 40){
+            //    foreach (string deneme in arkadasOlmayanlarınIlkkismi)
+            //    {
+            //        //DataReader'daki verileri parcalama a dındaki listte attım
+            //        arkadasOlanlar.Add(dr["ark"].ToString());
+            //        i++;
+            //    }
+            //}
+            ////veri oldugu sürece arkadasların aktivitesini arasın
+            //dr.Close();
+            //DataTable dtt = new DataTable();
+
+            //foreach (string parca in arkadasOlmayanlarınIlkkismi)
+            //{
+
+            //    string ara = "Select * From aktivite Where (OgrNo!=" + parca + ")";
+            //    SqlCommand komut2 = new SqlCommand(ara, baglanti);
+            //    SqlDataAdapter da = new SqlDataAdapter(komut2);
+            //    da.Fill(dt);
+
+            //}
+
+
+            //dataGridView2.DataSource = dt;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //SqlDataAdapter da = new SqlDataAdapter(komut);
+            //DataTable dt = new DataTable();
+
+            //da.Fill(dt);
+            //dataGridView1.DataSource = dt;
+            baglanti.Close();
+        }
+
+       
+    }
+}
