697d43e8e806c5245f8d06121d97c604965807c8
ünzile <unzile6167@gmail.com>
2018-05-31 01:15:47 +0300

    Add files via upload

---
+++ arkadasOneri/arkadasOneri/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace arkadasOneri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.muhendislikDataSet = new arkadasOneri.muhendislikDataSet();
+            this.aktiviteBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.aktiviteTableAdapter = new arkadasOneri.muhendislikDataSetTableAdapters.aktiviteTableAdapter();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.muhendislikDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aktiviteBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(372, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.Form1_Load);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SlateGray;
+            this.button1.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(607, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Arkadaş Öner";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // muhendislikDataSet
+            // 
+            this.muhendislikDataSet.DataSetName = "muhendislikDataSet";
+            this.muhendislikDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // aktiviteBindingSource
+            // 
+            this.aktiviteBindingSource.DataMember = "aktivite";
+            this.aktiviteBindingSource.DataSource = this.muhendislikDataSet;
+            // 
+            // aktiviteTableAdapter
+            // 
+            this.aktiviteTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.SteelBlue;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView2.Location = new System.Drawing.Point(5, 453);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(898, 18);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "EĞİTİM SETİ";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(12, 435);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "TEST SETİ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(163)))), ((int)(((byte)(252)))));
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.Location = new System.Drawing.Point(5, 108);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(898, 288);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(908, 474);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Arkadaş Öneri Sistemi";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.muhendislikDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aktiviteBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private muhendislikDataSet muhendislikDataSet;
+        private System.Windows.Forms.BindingSource aktiviteBindingSource;
+        private muhendislikDataSetTableAdapters.aktiviteTableAdapter aktiviteTableAdapter;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ arkadasOneri/arkadasOneri/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections;
+
+
+namespace arkadasOneri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //veritabanı baglantı cumlecigi veritabanı adı=muhendislik 
+        SqlConnection baglanti = new SqlConnection(@"server=UNZILE\SQLEXPRESS; Initial Catalog=muhendislik; Integrated Security=SSPI");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'muhendislikDataSet.aktivite' table. You can move, or remove it, as needed.
+            this.aktiviteTableAdapter.Fill(this.muhendislikDataSet.aktivite);
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open(); //veritabanı baglantısını actıkm
+            string[] arkadasOlanlar = new string[10];//girilen numaraya göre olusturulan arkadasların listesinin tutulacağı list
+            string[] arkadaslar1 = new string[18];
+            string[] arkadasOlmayanlarinBilgisi = new string[95];
+            for (int i = 0; i < 9; i++)
+            {
+                arkadasOlanlar[i] = "-";
+            }
+
+
+
+            string arkadaslar = "Select * From arkadaslar Where  (OgrNo=" + textBox1.Text + ")"; //girilen numaranın arkadaslarını cekecek sql sorgusu
+
+            SqlCommand komut = new SqlCommand(arkadaslar, baglanti); //sql sorgusunun veritabanı ile bağlantısını sağladm
+            SqlDataReader dr = komut.ExecuteReader();
+
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    //DataReader'daki verileri arkadasolanlar adındaki listte attım
+
+                    arkadasOlanlar[0] = dr["ark1"].ToString();
+                    arkadasOlanlar[1] = dr["ark2"].ToString();
+                    arkadasOlanlar[2] = dr["ark3"].ToString();
+                    arkadasOlanlar[3] = dr["ark4"].ToString();
+                    arkadasOlanlar[4] = dr["ark5"].ToString();
+                    arkadasOlanlar[5] = dr["ark6"].ToString();
+                    arkadasOlanlar[6] = dr["ark7"].ToString();
+                    arkadasOlanlar[7] = dr["ark8"].ToString();
+                    arkadasOlanlar[8] = dr["ark9"].ToString();
+                    arkadasOlanlar[9] = dr["ark10"].ToString();
+
+                }
+
+                
+            }
+        dr.Close();
+
+            //arkadasların aktivitelerin bulundugu kısım
+            DataTable dttt = new DataTable(); //verilerimi atacagım bir dataTable olusturdum
+            List<string> ogrenci = new List<string>();
+
+            foreach (string parca in arkadasOlanlar) // arkadaslarOlanlar listini gezinerek arkadaşların aktivitelerini getirilmesi
+            {
+
+                string ara = "Select * From aktivite Where (OgrNo=" + parca + ")"; //arkadasların öğrenci numaralarına göre aktivite bilgilerini cektim
+                SqlCommand komut22 = new SqlCommand(ara, baglanti); //veritabanı ile sql sorgusu arasında baglantıyı kurdum
+                SqlDataAdapter da1 = new SqlDataAdapter(komut22);
+                da1.Fill(dttt); //dataTable içerisini sql sorgusu sonucu gelen verilerle doldurdum
+                SqlDataReader dre1 = komut22.ExecuteReader();
+                dre1.Close(); 
+
+            }
+           
+            foreach(DataRow dre1 in dttt.Rows)
+            {
+                for(int i = 0; i < 17; i++)
+                {
+                    
+                    if (i != 16)
+                    {
+                        ogrenci.Add(dre1[i].ToString());
+                    }
+                    else
+                    {
+                        ogrenci.Add("1");
+                    }
+                   
+                }
+
+            }  
+   
+            //arkadas olmayanların aktivite bulma kısmı
+            string arkadasOlmayan = "SELECT * FROM liste WHERE not OgrNo=" + textBox1.Text + " AND " + " not OgrNo=" + arkadasOlanlar[0].ToString() + " AND " + "not OgrNo=" + arkadasOlanlar[1] + " AND " + "not OgrNo=" + arkadasOlanlar[2] + " AND " + "not OgrNo=" + arkadasOlanlar[3] + " AND " + "not OgrNo=" + arkadasOlanlar[4] + " AND " + "not OgrNo=" + arkadasOlanlar[5] + " AND " + "not OgrNo=" + arkadasOlanlar[6] + " AND " + "not OgrNo=" + arkadasOlanlar[7] + " AND " + "not OgrNo=" + arkadasOlanlar[8] + " AND " + "not OgrNo=" + arkadasOlanlar[9] + "";
+
+
+            SqlCommand dene = new SqlCommand(arkadasOlmayan, baglanti);
+            SqlDataReader drr = dene.ExecuteReader();
+            
+            int a = 0;
+            if (drr.HasRows)
+            {
+                while (drr.Read())
+                {
+
+                    arkadasOlmayanlarinBilgisi[a] = drr["OgrNo"].ToString();
+                    a++;
+                }
+            }
+            drr.Close();
+
+           
+
+            List<string> ogrenci2 = new List<string>();
+            DataTable data1 = new DataTable();
+
+            for (int c = 0; c < 40; c++)
+            // arkadaslarOlanlar listini gezinerek arkadaşların aktivitelerini getirilmesi
+            {
+               
+                string arama1 = "Select * From aktivite Where (OgrNo=" + arkadasOlmayanlarinBilgisi[c] + ")"; //arkadasların öğrenci numaralarına göre aktivite bilgilerini cektim
+                SqlCommand kmt2 = new SqlCommand(arama1, baglanti); //veritabanı ile sql sorgusu arasında baglantıyı kurdum
+                SqlDataAdapter daa2 = new SqlDataAdapter(kmt2);
+                daa2.Fill(data1); //dataTable içerisini sql sorgusu sonucu gelen verilerle doldurdum
+                SqlDataReader dre2 = kmt2.ExecuteReader();
+                dre2.Close();
+            }
+           
+            foreach (DataRow dre2 in data1.Rows)
+            {
+                for (int i = 0; i < 17; i++)
+                {
+
+                    if (i != 16)
+                    {
+                        ogrenci2.Add(dre2[i].ToString());
+                    }
+                    else
+                    {
+                        ogrenci2.Add("0");
+                    }
+                   // MessageBox.Show(ogrenci2[i].ToString());
+                }
+                
+            }
+            
+            
+
+
+            ogrenci.AddRange(ogrenci2); //arkadas olanların devamına arkadas olmayanları ekledim
+            
+            dataGridView1.ColumnCount = 17;
+
+            dataGridView1.Columns[0].Name = "id";
+            dataGridView1.Columns[1].Name = "OgrNo";
+            dataGridView1.Columns[2].Name = "aktv1";
+            dataGridView1.Columns[3].Name = "aktv2";
+            dataGridView1.Columns[4].Name = "aktv3";
+            dataGridView1.Columns[5].Name = "aktv4";
+            dataGridView1.Columns[6].Name = "aktv5";
+            dataGridView1.Columns[7].Name = "aktv6";
+            dataGridView1.Columns[8].Name = "aktv7";
+            dataGridView1.Columns[9].Name = "aktv8";
+            dataGridView1.Columns[10].Name = "aktv9";
+            dataGridView1.Columns[11].Name = "aktv10";
+            dataGridView1.Columns[12].Name = "aktv11";
+            dataGridView1.Columns[13].Name = "aktv12";
+            dataGridView1.Columns[14].Name = "aktv13";
+            dataGridView1.Columns[15].Name = "aktv15";
+            dataGridView1.Columns[16].Name = "etiket";
+
+            for (int i = 0; i < ogrenci.Count; i+=17)
+            {
+                
+                this.dataGridView1.Rows.Add(ogrenci[i], ogrenci[i+1], ogrenci[i+2], ogrenci[i+3], ogrenci[i+4], ogrenci[i+5], ogrenci[i+6], ogrenci[i+7], ogrenci[i+8], ogrenci[i+9], ogrenci[i+10], ogrenci[i+11], ogrenci[i+12], ogrenci[i+13], ogrenci[i+14], ogrenci[i+15], ogrenci[i+16]);
+            }
+
+
+
+
+
+
+            dataGridView2.ColumnCount = 17;
+
+            dataGridView2.Columns[0].Name = "id";
+            dataGridView2.Columns[1].Name = "OgrNo";
+            dataGridView2.Columns[2].Name = "aktv1";
+            dataGridView2.Columns[3].Name = "aktv2";
+            dataGridView2.Columns[4].Name = "aktv3";
+            dataGridView2.Columns[5].Name = "aktv4";
+            dataGridView2.Columns[6].Name = "aktv5";
+            dataGridView2.Columns[7].Name = "aktv6";
+            dataGridView2.Columns[8].Name = "aktv7";
+            dataGridView2.Columns[9].Name = "aktv8";
+            dataGridView2.Columns[10].Name = "aktv9";
+            dataGridView2.Columns[11].Name = "aktv10";
+            dataGridView2.Columns[12].Name = "aktv11";
+            dataGridView2.Columns[13].Name = "aktv12";
+            dataGridView2.Columns[14].Name = "aktv13";
+            dataGridView2.Columns[15].Name = "aktv15";
+            dataGridView2.Columns[16].Name = "etiket";
+
+
+            List<string> ogrenci3 = new List<string>();
+            DataTable sondata = new DataTable();
+            for (int k = 40; k < 80; k++)
+            // arkadasOlmayanlar listini gezinerek arkadaşların aktivitelerini getirilmesi
+            {
+
+                string son= "Select * From aktivite Where (OgrNo=" + arkadasOlmayanlarinBilgisi[k-1] + ")"; //arkadasların öğrenci numaralarına göre aktivite bilgilerini cektim
+                SqlCommand komutson = new SqlCommand(son, baglanti); //veritabanı ile sql sorgusu arasında baglantıyı kurdum
+                SqlDataAdapter sa = new SqlDataAdapter(komutson);
+                sa.Fill(sondata); //dataTable içerisini sql sorgusu sonucu gelen verilerle doldurdum
+                SqlDataReader sd = komutson.ExecuteReader();
+                sd.Close();
+            }
+            foreach (DataRow sd in sondata.Rows)
+            {
+                for (int i = 0; i < 17; i++)
+                {
+
+                    if (i != 16)
+                    {
+                        ogrenci3.Add(sd[i].ToString());
+                    }
+                    else
+                    {
+                        ogrenci3.Add("0");
+                    }
+                    
+                }
+
+            }
+            for (int i = 0; i < ogrenci3.Count; i += 17)
+            {
+
+                this.dataGridView2.Rows.Add(ogrenci3[i], ogrenci3[i+1], ogrenci3[i+2], ogrenci3[i+3], ogrenci3[i+4], ogrenci3[i+5], ogrenci3[i+6], ogrenci3[i+7], ogrenci3[i+8], ogrenci3[i+9], ogrenci3[i+10], ogrenci3[i+11], ogrenci3[i+12], ogrenci3[i+13], ogrenci3[i+14], ogrenci3[i+15], ogrenci3[i+16]);
+            }
+
+
+            baglanti.Close();
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e) //enter tusu ile arama yapabilmek icin
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(sender, e);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
+    
+
+
+
